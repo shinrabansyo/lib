6beb36add03b65377d095af1cc3f101afd665c98
wakuto <waku.902002@gmail.com>
2024-12-01 00:40:53 +0900

    gpio_writeの修正
    Co-authored-by: Yuta Nakagami <Yuta1004@users.noreply.github.com>
    Co-authored-by: Astalisks <Astalisks@users.noreply.github.com>
    Co-authored-by: Kota UCHIDA <uchan@uchan.net>
    Co-authored-by: Sato Koichiro <kouichi310@users.noreply.github.com>

--- gpio/write.asm
+++ gpio/write.asm
@@ -3,31 +3,55 @@
 ===
 // void  gpio_write(uint32_t pin, uint32_t value)
 @func_gpio_write
+    // r5 = 0x10: 0001_0000
+    // gpio =     0000_0000
+    // -------------------- or
+    //            0001_0000
     
-    // シフト量 ピン番号-1 を計算
+    // r5 = 0xef: 1110_1111
+    // gpio =     0001_0000
+    // -------------------- and
+    //            0000_0000
+    
+    // r4 = シフト量
     add r4 = r0, r10
-    subi r4 = r4, 1
+    addi r5 = r0, 1
+
 
     // 書き込む値を決定
-    beq r0, (r11, r0) -> @zero.func_gpio_write
-    addi r5 = r0, 1
-    beq r0, (r0, r0) -> @zero_end.func_gpio_write
-    @zero.func_gpio_write
-        addi r5 = r0, 0
-    @zero_end.func_gpio_write
+    beq r0, (r11, r0) -> @low.func_gpio_write
+    beq r0, (r0, r0) -> @high.func_gpio_write
+    // lowを書き込む処理
+    @low.func_gpio_write
+        // マスク準備
+        sll r4 = r5, r4
+        xori r5 = r4, 0xFFFFFFFF
 
-    // マスク準備
-    srl r4 = r5, r4
-    xori r5 = r4, 0xFFFFFFFF
+        // GPIOの現在状態を取得
+        in r6 = r0[4]
+        
+        // 現在状態にマスクをかける
+        and r7 = r5, r6
 
-    // GPIOの現在状態を取得
-    in r6 = r0[4]
+        // GPIOの現在状態を更新
+        out r0[4] = r7
 
-    // 現在状態にマスクをかける
-    and r7 = r5, r6
+        // return
+        jal r0, r1[0]
 
-    // GPIOの現在状態を更新
-    out r0[4] = r7
+    // highを書き込む処理
+    @high.func_gpio_write
+        // マスク準備
+        sll r4 = r5, r4
 
-    // return
-    jal r0, r1[0]
+        // GPIOの現在状態を取得
+        in r6 = r0[4]
+        
+        // 現在状態にマスクをかける
+        or r7 = r4, r6
+
+        // GPIOの現在状態を更新
+        out r0[4] = r7
+    
+        // return
+        jal r0, r1[0]
